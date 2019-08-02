bd04f91fed93ef1baba00031992ea12dd3e801a9
dede53 <hallomarcel.hm@gmail.com>
2019-08-02 17:41:07 +0200

    add Prototypes, but should use class{}

--- allAdapter.js
+++ allAdapter.js
@@ -1,5 +1,5 @@
 var fs						=	require('fs');
-var adapterStatus = function(data, name){
+var adapterStatus 			= function(data, name){
 
 	var path							= __dirname + '/adapter/' + name + "/index.js";
 	var debugFile						= __dirname + '/log/debug-' + name + '.log';
@@ -13,7 +13,7 @@ var adapterStatus = function(data, name){
 		installedVersion: "",
 		availableVersion: data.version,
 		description: data.decription,
-		hasWebpage:"",
+		hasWebpage: false,
 		inProcess: false
 	},
 	this.logFile = fs.createWriteStream( debugFile, {flags : 'w', encoding: 'utf8'});
@@ -46,4 +46,14 @@ var adapterStatus = function(data, name){
 	}
 }
 
+// Should use class{}
+var allAdapter = function(){}
+allAdapter.prototype = new events.EventEmitter();
+allAdapter.prototype.start 		= function(name) {};
+allAdapter.prototype.stop 		= function(name) {};
+allAdapter.prototype.restart 	= function(name) {};
+allAdapter.prototype.update 	= function(name) {};
+allAdapter.prototype.install 	= function(name) {};
+allAdapter.prototype.remove	 	= function(name) {};
+allAdapter.prototype.send	 	= function(name) {};
 module.exports = adapterStatus;
