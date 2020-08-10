21506a32db079424fe4cd3beb4e8fe29e8c5d251
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2020-08-11 00:26:08 +0200

    move to express.oi by dede53

--- adapterFunctions.js
+++ adapterFunctions.js
@@ -1,4 +1,4 @@
-var express					=	require('express.moi');
+var express					=	require('express.oi');
 var child_process			=	require('child_process');
 var bodyParser				=	require('body-parser');
 var request				    =	require('request');
